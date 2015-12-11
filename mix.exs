f02c0a5502c55242e19615878802ca9f386da3f5
Yury Gargay <yury.gargay@gmail.com>
2015-12-11 14:22:18 +0600

    Add hex.pm

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Medex.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package]
   end
 
   def application do
@@ -19,5 +21,16 @@ defmodule Medex.Mixfile do
     [{:cowboy, "~> 1.0.0"},
      {:plug, "~> 1.0"},
      {:consul, ">= 1.0.0", optional: true}]
+  end
+
+  defp description do
+    """
+    Medical Examination - application for register health check callbacks and represent their state via HTTP.
+    """
+  end
+
+  defp package do
+    [maintainers: ["Yury Gargay"],
+     links: %{"GitHub" => "https://github.com/xerions/medex"}]
   end
 end
